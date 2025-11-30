57e6acd146cc4482f6e4dea891f2651acf437b77
Kyle King <KyleKing@users.noreply.github.com>
2025-11-29 22:03:22 -0600

    ci: fix Windows support

--- tests/test_mdformat.py
+++ tests/test_mdformat.py
@@ -6,9 +6,9 @@ import mdformat
 def test_mdformat_text():
     """Verify that using mdformat works as expected."""
     pth = Path(__file__).parent / "pre-commit-test.md"
-    content = pth.read_text()
+    content = pth.read_text(encoding="utf-8")
 
     result = mdformat.text(content, extensions={"slw"})
 
-    pth.write_text(result)  # Easier to debug with git
+    pth.write_text(result, encoding="utf-8")  # Easier to debug with git
     assert result == content, "Differences found in format. Review in git."
